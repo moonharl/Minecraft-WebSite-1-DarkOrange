70c18797f7a6ea1ae526757850c964eefc95cb2f
moonharl <127766078+moonharl@users.noreply.github.com>
2023-03-18 19:05:47 -0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -3,7 +3,7 @@ window.onload = function () {
     // GENERAL CONFIGS v0.1
 
     //Site title
-    let title = 'BattleCraft';
+    let title = ' ';
 
     //Server Ip (counter)
     var serverIp = 'play.foxcraft.net';
